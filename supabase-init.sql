1e593cb076bcd8211f1bf29e08ecb1f86064fe1b
Guilherme Souza <guilherme@grds.dev>
2022-05-10 09:39:30 -0300

    Add RLS

--- supabase-init.sql
+++ supabase-init.sql
@@ -11,3 +11,10 @@ create table logs (
     received_at timestamp with time zone default timezone('utc'::text, now()) not null,
     metadata jsonb
 );
+
+alter table logs enable row level security;
+
+create policy "Logs can be public inserted." on public.logs for insert with check (true);
+create policy "Logs can't be public read." on public.logs for select using (false);
+create policy "Logs can't be public updated." ON public.logs for update with check (false);
+create policy "Logs can't be public deleted." ON public.logs for delete using (false);
